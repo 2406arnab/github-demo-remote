caee04a020da607d10b96b67d15d06f351ad73d9
arnab_ghosh <2406arnab@gmail.com>
2022-02-23 21:41:23 +0530

    Added all function header

--- arnab.py
+++ arnab.py
@@ -1,9 +1,12 @@
 #Add implementation
 def add(x,y):
     pass
+#Substract implementation
 def substract(x,y):
     fail
+#multiply implementation
 def multiply(x,y):
     pass
+#Divide implementation
 def divide(x,y):
     fail
